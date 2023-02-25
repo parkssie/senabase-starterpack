e0cd4e0fe35be064e1055ab0e59f5774ec22d723
Jungkyu Park <parkssie@gmail.com>
2023-02-25 18:13:50 +0900

    Update update setup.py, changed python_requires to >=3.10

--- setup.py
+++ setup.py
@@ -6,14 +6,14 @@ this_directory = Path(__file__).parent
 long_description = (this_directory / 'README.md').read_text()
 
 setup(name='senabase-starterpack',
-      version='0.1.4',
+      version='0.1.5',
       description='starterpack library',
       long_description=long_description,
       long_description_content_type='text/markdown',
       author='Jungkyu Park',
       author_email='parkssie@gmail.com',
       url='https://github.com/parkssie/senabase-starterpack',
-      python_requires='>=3.11',
+      python_requires='>=3.10',
       packages=find_packages("./", exclude=["prototypes"]),
       install_requires=[
           'psycopg2-binary>=2.9.*',
